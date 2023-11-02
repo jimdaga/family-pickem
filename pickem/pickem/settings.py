0bdf7ce883a13fe483edda7d6d5cb8b98e5f060c
jimdaga <jimdagacodez@gmail.com>
2023-11-02 09:06:16 -0400

    test fix

--- pickem/pickem/settings.py
+++ pickem/pickem/settings.py
@@ -209,7 +209,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'pickem_api/static/'),]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
